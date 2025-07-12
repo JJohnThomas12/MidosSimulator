f19f563e688aa541b5740ad26233653995fe157c
Joel John Thomas <joeljt2468@gmail.com>
2025-07-11 22:15:38 -0500

    Update Scheduler.cpp

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -1,13 +1,3 @@
-// ***************************************************************************
-//
-//   Joel John Thomas 
-//   Z2004783
-//   CSCI 480 - PE1 
-//
-//   I certify that this is my own work and where appropriate an extension 
-//   of the starter code provided for the assignment. 
-// ***************************************************************************
-
 #include "Scheduler.h"
 
 // Add a new process to the scheduler’s process list
