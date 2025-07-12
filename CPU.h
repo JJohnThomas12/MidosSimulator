76df09f2ce2d83161683decd80d0a198d36c6d7d
Joel John Thomas <joeljt2468@gmail.com>
2025-07-11 22:13:15 -0500

    Update and rename CPU (2).h to CPU.h

--- CPU.h
+++ CPU.h
@@ -1,13 +1,3 @@
-// ***************************************************************************
-//
-//   Joel John Thomas 
-//   Z2004783
-//   CSCI 480 - PE1 
-//
-//   I certify that this is my own work and where appropriate an extension 
-//   of the starter code provided for the assignment. 
-// ***************************************************************************
-
 #ifndef CPU_H
 #define CPU_H
 
